723d3197a8fe1682ec26a1190246ba2bdd74b8a6
knox <knoxfighter@asdff45.de>
2025-09-22 18:15:19 +0200

    add Description & optional Copyright & optional VersionStrings into VersionInfo

--- cmake/vs-version.cmake
+++ cmake/vs-version.cmake
@@ -1,7 +1,21 @@
 function(generate_version_file)
 	# set() all needed variables here, they are only set in function scope
 	set(options)
-	set(oneValueArgs VERSION_MAJOR VERSION_MINOR VERSION_PATCH VERSION_REF INTERNAL_NAME COPYRIGHT ORIGINAL_FILENAME PRODUCT_NAME)
+	set(oneValueArgs
+			VERSION_MAJOR
+			VERSION_MINOR
+			VERSION_PATCH
+			VERSION_REF
+			VERSION_MAJOR_STR
+			VERSION_MINOR_STR
+			VERSION_PATCH_STR
+			VERSION_REF_STR
+			INTERNAL_NAME
+			COPYRIGHT
+			ORIGINAL_FILENAME
+			PRODUCT_NAME
+			DESCRIPTION
+	)
 	set(multiValueArgs)
 
 	# cmake_parse_arguments
@@ -24,12 +38,25 @@ function(generate_version_file)
 		message(FATAL_ERROR "VERSION_REF has to be defined!")
 	endif ()
 
+	if (NOT DEFINED PROJECT_VERSION_MAJOR_STR)
+		set(PROJECT_VERSION_MAJOR_STR "${PROJECT_VERSION_MAJOR}")
+	endif ()
+	if (NOT DEFINED PROJECT_VERSION_MINOR_STR)
+		set(PROJECT_VERSION_MINOR_STR "${PROJECT_VERSION_MINOR}")
+	endif ()
+	if (NOT DEFINED PROJECT_VERSION_PATCH_STR)
+		set(PROJECT_VERSION_PATCH_STR "${PROJECT_VERSION_PATCH}")
+	endif ()
+	if (NOT DEFINED PROJECT_VERSION_REF_STR)
+		set(PROJECT_VERSION_REF_STR "${PROJECT_VERSION_REF}")
+	endif ()
+
 	if (NOT DEFINED PROJECT_INTERNAL_NAME)
 		message(FATAL_ERROR "INTERNAL_NAME has to be defined!")
 	endif ()
 
 	if (NOT DEFINED PROJECT_COPYRIGHT)
-		message(FATAL_ERROR "COPYRIGHT has to be defined!")
+		set(PROJECT_COPYRIGHT "")
 	endif ()
 
 	if (NOT DEFINED PROJECT_ORIGINAL_FILENAME)
@@ -38,6 +65,10 @@ function(generate_version_file)
 
 	if (NOT DEFINED PROJECT_PRODUCT_NAME)
 		message(FATAL_ERROR "PRODUCT_NAME has to be defined!")
+	endif ()
+
+	if (NOT DEFINED PROJECT_DESCRIPTION)
+		set(PROJECT_DESCRIPTION "")
 	endif ()
 
 	configure_file("${CMAKE_CURRENT_FUNCTION_LIST_DIR}/VersionInfo.rc.in" "${CMAKE_CURRENT_BINARY_DIR}/VersionInfo.rc" @ONLY)
