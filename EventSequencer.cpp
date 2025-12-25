f7e530cb8fccc5520f24abfd01e36f749b61702c
knox <knoxfighter@asdff45.de>
2025-12-25 19:21:40 +0100

    dont sleep the thread, let a condition variable do it && run all events on a single consumer
    Co-authored-by: jackpoz <giacomopoz@gmail.com>

--- EventSequencer.cpp
+++ EventSequencer.cpp
@@ -2,22 +2,12 @@
 
 #include <algorithm>
 #include <chrono>
+#include <utility>
 
 void ArcdpsExtension::EventSequencer::ProcessEvent(cbtevent* pEv, ag* pSrc, ag* pDst, const char* pSkillname, uint64_t pId, uint64_t pRevision) {
 	std::lock_guard guard(mElementsMutex);
-	if (pId == 0) {
-		// if no events are queued call id=0 events directly
-		if (mElements.empty()) {
-			mCallback(pEv, pSrc, pDst, pSkillname, pId, pRevision);
-			return;
-		}
-
-		// insert it in the prio queue
-		mElements.emplace(pEv, pSrc, pDst, pSkillname, mLastId, pRevision);
-	} else {
-		mLastId = pId;
-		mElements.emplace(pEv, pSrc, pDst, pSkillname, pId, pRevision);
-	}
+	mElements.emplace(pEv, pSrc, pDst, pSkillname, pId, pRevision);
+	mNewElement.notify_all();
 }
 
 bool ArcdpsExtension::EventSequencer::EventsPending() const {
@@ -28,20 +18,28 @@ void ArcdpsExtension::EventSequencer::Reset() {
 	std::unique_lock guard(mElementsMutex);
 	mElements.clear();
 	mNextId = 2;
-	mLastId = 2;
 }
 
-ArcdpsExtension::EventSequencer::EventSequencer(const CallbackSignature& pCallback) : mCallback(pCallback) {
+ArcdpsExtension::EventSequencer::EventSequencer(CallbackSignature pCallback) : mCallback(std::move(pCallback)) {
 	using namespace std::chrono_literals;
 	mThread = std::jthread([this](std::stop_token stoken) {
+		std::unique_lock guard(mElementsMutex, std::defer_lock);
 		while (!stoken.stop_requested()) {
-			while (!stoken.stop_requested() && !mElements.empty()) {
-				Runner();
-			}
-
+			guard.lock();
+			mNewElement.wait(guard, stoken, [this] {
+				return !mElements.empty() && mElements.begin()->Id <= mNextId;
+			});
 			if (stoken.stop_requested()) return;
 
-			std::this_thread::sleep_for(100ms);
+			// If we get here, the predicate is already checked and we can assume that mElements is not empty
+			auto item = mElements.extract(mElements.begin());
+			guard.unlock();
+			Event& event = item.value();
+			EventInternal(event);
+
+			if (event.Id == mNextId) {
+				++mNextId;
+			}
 		}
 	});
 }
@@ -50,7 +48,7 @@ ArcdpsExtension::EventSequencer::~EventSequencer() {
 	Shutdown();
 }
 
-void ArcdpsExtension::EventSequencer::EventInternal(Event& pElem) {
+void ArcdpsExtension::EventSequencer::EventInternal(Event& pElem) const {
 	cbtevent* event = nullptr;
 	if (pElem.Ev.Present) {
 		event = &pElem.Ev;
